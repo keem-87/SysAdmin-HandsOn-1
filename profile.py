67753aa19011553192218a8be4b688041105fa84
Rackeem Reid <78712819+keem-87@users.noreply.github.com>
2022-07-21 19:46:55 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -16,7 +16,7 @@ for i in range(2):
     
   node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
-  iface = node.addInterface("if" = str(i))
+  iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
   iface.addAddress(rspec.IPv4Address(prefixForIP = str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
