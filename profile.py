ac30f693e2eadf923676fb9deee227c4b96012b3
Rackeem Reid <78712819+keem-87@users.noreply.github.com>
2022-07-21 19:42:31 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -14,15 +14,15 @@ for i in range(2):
   else:
       node = request.XenVM("observer")
     
-    node.routable_control_ip = "true"
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
-    iface = node.addInterface("if" = str(i))
-    iface.component_id = "eth1"
-    iface.addAddress(rspec.IPv4Address(prefixForIP = str(i + 1), "255.255.255.0"))
-    link.addInterface(iface)
+  node.routable_control_ip = "true"
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+  iface = node.addInterface("if" = str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(rspec.IPv4Address(prefixForIP = str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
     
-    if i == 0:
-      node.addService(rspec.Execute(shell="sh" , command= "sudo bash /local/repository/setup_apache.sh"))
+  if i == 0:
+    node.addService(rspec.Execute(shell="sh" , command= "sudo bash /local/repository/setup_apache.sh"))
       
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
