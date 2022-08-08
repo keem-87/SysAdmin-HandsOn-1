c1f669c67d423d44b21a119c0ed521d76c132fdf
Rackeem Reid <78712819+keem-87@users.noreply.github.com>
2022-08-07 23:18:16 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+sudo apt-get install ssmpt > /dev/null
+sudo apt-get install mailutils > /dev/null
+
+NOW=$(date | cut -c1-13)
+hrAgo=$(date -d "1 hours ago" | cut -c1-13)
+lTime=$(date -r /var/webserver_monitor/unauthorized.log | cut -c1-17)
+if [ "$hrAgo" == "$lTime" ]; then
+        contents = "No unauthorized attempts."
+else
+        contents =$(sudo sed -n '/"$hrAgo"/,/"$NOW"/p' /var/webserver_monitor/unauthorized.log)
+fi
+echo "No unauthorized access!!!" | mail -s "Unauthorized.log Hourly Update" RR935981@wcupa.edu -A "$contents"
